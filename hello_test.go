43d173a1633d63be2e348ba2832e413d26c9a97e
Asynkoo <asynkoo9@gmail.com>
2025-04-27 00:12:06 +0900

    fix: refactoring

--- hello_test.go
+++ hello_test.go
@@ -7,17 +7,21 @@ func TestHello(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello to empty string", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
