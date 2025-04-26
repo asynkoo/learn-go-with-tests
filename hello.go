b4dd99119cf04875ddc1da308b9f99ecbfe47052
Asynkoo <asynkoo9@gmail.com>
2025-04-27 00:25:06 +0900

    refac: refactoring

--- hello.go
+++ hello.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
-const greeting = "Hello, "
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return greeting + name
+	prefix := englishHelloPrefix
+
+	return greetingPrefix(language) + name
+
+	return prefix + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", "english"))
 }
